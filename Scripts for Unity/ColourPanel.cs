300e3ab303359b38f18dd8137b80dd16a7053b31
Rutherfordum <amaterasu.dzen@gmail.com>
2019-01-13 19:26:02 +0300

    add new scripts ColorPanel

---
+++ Scripts for Unity/ColourPanel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ColourPanel : MonoBehaviour {
+	private Color[] Col=new Color[3];
+	public  float LerpSpeed=1;
+	private float _time;
+
+	void Update() {
+		_time += Time.deltaTime;
+		Col[0] = Color.Lerp (Col[1], Col[2], _time* LerpSpeed);
+		if (Col[0] == Col[2]) {
+			Col[1]=Col[2];
+			Col[2] = new Color (Random.value,Random.value,Random.value);
+			_time=0;
+		}
+		GetComponent<Image> ().color = Col[0];
+		}
+	}
